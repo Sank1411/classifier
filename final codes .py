8b38f713f776edfa59a044224d27c3f9f85fa8ff
Sanket Gajbhiye <40202820+Sank1411@users.noreply.github.com>
2019-07-17 14:13:32 +0530

    Update final codes .py

--- final codes .py
+++ final codes .py
@@ -40,12 +40,6 @@ def preprocessing(path,path_csv,no_of_img,no_of_fliped):#'C:/Users/SANKET/Deskto
 # preprocessing('C:/Users/SANKET/Desktop/classifier/trial_dataset','C:/Users/SANKET/Desktop/classifier/trial_sedanvssuv.csv',240,100)
 
 
-# In[25]:
-
-
-print(a)
-
-
 # In[26]:
 
 
@@ -82,9 +76,6 @@ def batch_formation(batch_size,List,path_all):
 def weight_biases(batch_size):
     w = np.random.normal(0.0,1.0,(180000,1))
     b = np.random.normal(0.0,1.0,(batch_size,1))
-#     print(w,'\n')
-#     print(b)
-#     print(w.shape)
     return w,b
 # weight_biases(20)
 
@@ -94,12 +85,7 @@ def weight_biases(batch_size):
 
 def forward_pass(w,b,x):
     import numpy as np
-#     (w,b) = weight_biases()
-#     (x,m)= batch_formation(20,preprocessing('C:/Users/SANKET/Desktop/1'))
     y = np.add(np.dot(x,w),b)
-#     matmul use karna hai
-#     print(np.shape(y))
-#     print(y)
     return y
 # forward_pass()
 
@@ -110,8 +96,6 @@ def forward_pass(w,b,x):
 import numpy as np
 def sigmoid(x,batch_size):
     y = np.empty([batch_size,1])
-#     g = np.empty([20,1])
-#     k = 0
     g = 1/(1+np.exp(-x))
     for i in range(batch_size) :
         if g[i] >= 0.5:
@@ -127,15 +111,11 @@ def sigmoid(x,batch_size):
 
 import numpy as np
 def cost_function(batch_size,w,b,path):
-#     f = preprocessing('C:/Users/SANKET/Desktop/1','C:/Users/SANKET/Desktop/dataset_sedanvssuv.csv')
     (x,y) = batch_formation(batch_size,a,path)
-#     (w,b) = weight_biases(batch_size,w_new,b_new)
     (h,t) = sigmoid(forward_pass(w,b,x),batch_size)
     A = -(y*np.log(h)+(1-y)*np.log(h))
     cost_total = np.sum(A)
     cost_avg = cost_total/(batch_size)
-#     print(A,cost_total,cost_avg)
-#     print(A.shape)
     return A,cost_total,cost_avg
 # cost_function(20)
 
@@ -145,14 +125,11 @@ def cost_function(batch_size,w,b,path):
 
 import numpy as np
 def gradient_descent(m,A,batch_size,w,b,path):
-#     (w,b) = weight_biases(batch_size<w_new,b_new)
-#     f = preprocessing('C:/Users/SANKET/Desktop/1','C:/Users/SANKET/Desktop/dataset_sedanvssuv.csv')
     (x,y)= batch_formation(batch_size,a,path)
     (g) = forward_pass(w,b,x)
     (h,e) = sigmoid(g,batch_size)
     w = w - (A/m)*np.matmul(np.transpose(x),(h-y))
     b = b - (A/m)*(h-y)
-#     print(np.shape(w),np.shape(b))
     return w,b
 # gradient_descent(100,0.0001) 
 
@@ -162,8 +139,6 @@ def gradient_descent(m,A,batch_size,w,b,path):
 
 def accuracy(batch_size,w,b,path):
     count = 0
-#     (w,b) = weight_biases(batch_size,w_new,b_new)
-#     temp_2 = preprocessing('C:/Users/SANKET/Desktop/1','C:/Users/SANKET/Desktop/dataset_sedanvssuv.csv')
     (e,f) = batch_formation(batch_size,a,path)
 #     (batch1,lable2) = batch_formation(20,temp_2,'C:/Users/SANKET/Desktop/1')
     (h,y) = sigmoid(forward_pass(w,b,e),batch_size)
@@ -172,7 +147,6 @@ def accuracy(batch_size,w,b,path):
         if temp[i]!=0:
             count = count +1
     s = (1-(count/20))*100
-#     print(s)
     return s
 # accuracy()
 
